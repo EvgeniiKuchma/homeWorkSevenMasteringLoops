111cdd5e3d83028976370ba6ee32b9a24256f9d0
Евгений Кучма <desferto87@gmail.com>
2024-06-19 21:50:28 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkSevenMasteringLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberLoop;
+            Console.Write("Введите количество повторений: ");
+            numberLoop = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 1; i <= numberLoop; i++)
+            {
+                Console.WriteLine("Итерация № " + i);
+            }
+        }
+    }
+}
